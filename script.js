95c275d15260aa3bac075947e7b253265fb09ee4
KarlaDiazE <89496730+KarlaDiazE@users.noreply.github.com>
2021-11-16 18:17:43 -0300

    added forecast API

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast () {
+function displayForecast (response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   
   let forecastHTML = `<div class="row">`;
@@ -46,6 +47,14 @@ function displayForecast () {
     forecastElement.innerHTML = forecastHTML; 
 }
 
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "250594dd1dfd2f21605c69d6c68a5cd9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
@@ -63,12 +72,15 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
   let apiKey = "250594dd1dfd2f21605c69d6c68a5cd9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+ 
 }
 
 function handleSubmit(event) {
@@ -110,4 +122,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 searchCity("Santiago");
-displayForecast ();
